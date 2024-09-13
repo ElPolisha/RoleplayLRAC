aa5b22fdb1ec736b6887da746a329291001b28a3
Agustin <agustincigaran@icloud.com>
2024-09-13 12:43:27 -0300

    cree la clase de elementos

---
+++ src/Library/Elementos.cs
@@ -0,0 +1,24 @@
+namespace Library;
+
+public class Elementos
+{
+    private int Durabilidad = int;
+    private int Daño_fisico = int;
+    private double Cantidad = double;
+    private int Magia = int;
+    private int Daño_magico = int;
+    private string Conocimiento_magico = string;
+    
+    public Elementos (int durabilidad, int daño_fisico)
+    {
+        this.Durabilidad = durabilidad;
+        this.Daño_fisico = daño_fisico;
+        Console.Write($"Usted creo un martillo con {durabilidad} durabilidad y {daño_fisico} puntos de daño.\n")
+        return Daño
+    }
+
+    public void martillo()
+    {
+        
+    }
+}
