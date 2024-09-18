0eaa5e4918eed6790ba0c19f640a04689dde62dd
Agustin <agustincigaran@icloud.com>
2024-09-18 20:48:59 -0300

    cree los test de los metodos dentro de personaje, las otras clases no necesitan test a mi parecer, rider no me deja runear los test pero quedaron hechos

--- test/LibraryTests/PersonajesTest.cs
+++ test/LibraryTests/PersonajesTest.cs
@@ -2,13 +2,45 @@ namespace LibraryTests;
 
 public class PersonajesTest
 {
-	/*[Test]
+    [Test]
+    public void presentarseTest()
+    {
+        Mago x = new Mago("Pepe");
+        string nombreObtenido = x.presentarse;
+        string nombreEsperad = "Pepe";
+
+        Assert.AreEqual(nombreEsperad, nombreObtenido);
+    }
+    
+    [Test]
     public void pelearenserioTest(Personajes x, Personajes y)
     {
         Mago x = new Mago("juancito");
         Mago y = new Mago("pepito");
-        int vida_esperada();
-        int vida_Restante = (x);
+        int vida_esperada = 26;  //daño total mago estandar 83 & defensa 9
+        int vida_Restante = (y.vida + y.Defensa) - (x.Daño);
+
+        Assert.AreEqual(vida_esperada, vida_Restante);
     }
-*/
+
+    [Test]
+    public void pelearTest()
+    {
+        Mago x = new Mago("pepe");
+        Elementos baston = new Elementos("baston", "Mago", 0, 0);
+        int dañoObtenido = x.pelear;
+        int dañoEsperado = 83;
+
+        Assert.AreEqual(dañoEsperado,dañoObtenido);
+    }
+
+    public void defenderseTest()
+    {
+        Mago x = new Mago("jabonete");
+        Elementos baston = new Elementos("baston", "Mago", 0, 0);
+        int defensaObtenida = x.defenderse;
+        int defensaEsperada = 9;
+
+        Assert.AreEqual(defensaEsperada,defensaObtenida);
+    }
 }
