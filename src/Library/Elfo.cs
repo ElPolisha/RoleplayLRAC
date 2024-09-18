6d5313d061d545790d3dc58c64a010d041cdeac7
bernaochoa <166560678+bernaochoa@users.noreply.github.com>
2024-09-18 19:25:47 -0300

    Elfo y gemas magicas

---
+++ src/Library/Elfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+using System;
+using System.Collections.Generic;
+namespace Library;
+
+public class Elfo : Personajes
+{
+    private int Sentido_aumentado = 50;
+    public Gemas_magicas gema1 = new Gemas_magicas("Esmeralda", "Elfo", 25, 25);
+
+    public Elfo(string nombre)
+    {
+        this.armas = new List<Elementos>();
+        this.nombre = nombre;
+        //this.estadisticasGema();
+        Console.WriteLine($"{this.nombre} es un escudero, tiene {this.Daño} puntos de ataque, {this.Defensa} puntos de defensa y {this.vida} puntos de vida");
+    }
+    
+    public void agregararmaElfo(Elementos cosa)
+    {
+        if (cosa.Tipo == Tipo.Elfo)
+        {
+            armas.Add(cosa);
+        }
+        else
+        {
+            Console.WriteLine($"Solo se puede agregar armas de elfo, a un elfo");
+        }
+    }
+
+    private void estadisticasGema()
+    {
+        this.Daño = Daño + gema1.GetDaño();
+        this.Defensa = Defensa + gema1.GetDefensa();
+    }
+}
---
+++ src/Library/Gemas_magicas.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace;
+
+public class Gemas_magicas : Elementos
+{
+    public Gemas_magicas(string nombrearma, string tipo, int durabilidad, int daño_fisico) : base(nombrearma, tipo, durabilidad,
+        daño_fisico)
+    {
+        this.nombre_arma = nombrearma;
+        tipo = "Elfo";
+        this.defensa_elemento = durabilidad;
+        this.daño_elemento = daño_fisico;
+    }
+}
