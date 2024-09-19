7c37c833f583ce9c3c77f5027a2509da2bb2edb3
Agustin <agustincigaran@icloud.com>
2024-09-18 21:09:20 -0300

    Bueno, termine de armas los test, pero rider sigue sin dejarme runearlos. mas que eso no puedo hacer

--- test/LibraryTests/PersonajesTest.cs
+++ test/LibraryTests/PersonajesTest.cs
@@ -6,21 +6,21 @@ public class PersonajesTest
     public void presentarseTest()
     {
         Mago x = new Mago("Pepe");
-        string nombreObtenido = x.presentarse;
+        string nombreObtenido = $"{x.presentarse}";
         string nombreEsperad = "Pepe";
 
         Assert.AreEqual(nombreEsperad, nombreObtenido);
     }
     
     [Test]
-    public void pelearenserioTest(Personajes x, Personajes y)
+    public void pelearenserioTest()
     {
-        Mago x = new Mago("juancito");
-        Mago y = new Mago("pepito");
+        Mago atacante = new Mago("juancito");
+        Mago atacado = new Mago("pepito");
         int vida_esperada = 26;  //daño total mago estandar 83 & defensa 9
-        int vida_Restante = (y.vida + y.Defensa) - (x.Daño);
+        atacante.pelearseenserio(atacante, atacado);
 
-        Assert.AreEqual(vida_esperada, vida_Restante);
+        Assert.AreEqual(vida_esperada, atacado.vida);
     }
 
     [Test]
@@ -28,7 +28,8 @@ public class PersonajesTest
     {
         Mago x = new Mago("pepe");
         Elementos baston = new Elementos("baston", "Mago", 0, 0);
-        int dañoObtenido = x.pelear;
+        x.agregararmamago(baston);
+        int dañoObtenido = x.pelear();
         int dañoEsperado = 83;
 
         Assert.AreEqual(dañoEsperado,dañoObtenido);
@@ -38,7 +39,8 @@ public class PersonajesTest
     {
         Mago x = new Mago("jabonete");
         Elementos baston = new Elementos("baston", "Mago", 0, 0);
-        int defensaObtenida = x.defenderse;
+        x.agregararmamago(baston);
+        int defensaObtenida = x.defenderse();
         int defensaEsperada = 9;
 
         Assert.AreEqual(defensaEsperada,defensaObtenida);
